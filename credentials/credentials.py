c5db4f6d8f622998fc3773eac8b5bd2420e0a78d
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2020-11-22 16:56:08 +0900

    function形式に変更

--- credentials/credentials.py
+++ credentials/credentials.py
@@ -2,10 +2,12 @@ import boto3
 import pprint
 
 # デフォルト
-sts = boto3.client('sts')
-pprint.pprint(sts.get_caller_identity())
+def default_credential():
+  sts = boto3.client('sts')
+  pprint.pprint(sts.get_caller_identity())
 
 # profile 使用
-session = boto3.Session(profile_name='default')
-sts = session.client('sts')
-pprint.pprint(sts.get_caller_identity())
+def use_profile():
+  session = boto3.Session(profile_name='default')
+  sts = session.client('sts')
+  pprint.pprint(sts.get_caller_identity())
